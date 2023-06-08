55fbd8afca76d2ce0a3aadf34daf46e2f0cecdd4
njindal <jindal.nipun@gmail.com>
2023-06-08 21:46:21 +0530

    Top bar

--- app.py
+++ app.py
@@ -2,21 +2,36 @@
 import os
 from flask import Flask, render_template
 import datetime
+import pytz
 
 app = Flask(__name__)
+
+
+def read_csv_data(csv_path):
+    with open(csv_path, 'r') as file:
+        csv_data = file.read()
+    return csv_data
+
+
+def get_modified_date(csv_path):
+    last_modified = os.path.getmtime(csv_path)
+    modified_datetime = datetime.datetime.fromtimestamp(last_modified)
+    ist_timezone = pytz.timezone("Asia/Kolkata")
+    modified_datetime_ist = modified_datetime.astimezone(ist_timezone)
+    modified_date = modified_datetime_ist.strftime("%a, %b %d, %Y, %Z %I:%M %p")
+    return modified_date
 
 
 @app.route('/')
 def index():
     # Read data from CSV file
     csv_path = 'data/data.csv'
-    with open(csv_path, 'r') as file:
-        csv_data = file.read()
+    csv_data = read_csv_data(csv_path)
 
     # Get last modified date of CSV file
-    last_modified = os.path.getmtime(csv_path)
+    modified_date = get_modified_date(csv_path)
 
-    return render_template('index.html', csv_data=csv_data, last_modified=last_modified)
+    return render_template('index.html', csv_data=csv_data, modified_date=modified_date)
 
 
 if __name__ == '__main__':
---
+++ prompt.sh
@@ -0,0 +1,21 @@
+echo "You are an expert in Python and HTML. Please use latest constructs of Python and HTML."
+echo ""
+echo "I am writing a Flask based application."
+
+echo ""
+echo ""
+
+echo "Please share the modified or added code."
+
+echo ""
+echo ""
+
+echo "'''"
+cat app.py
+echo "'''"
+
+echo ""
+
+echo "'''"
+cat templates/index.html
+echo "'''"
