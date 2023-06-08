21286f34f2c3192e6552da923c1b5139148fe97c
njindal <jindal.nipun@gmail.com>
2023-06-08 20:11:05 +0530

    Viewer application

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+# app.py
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    # Read data from CSV file
+    with open('data/data.csv', 'r') as file:
+        csv_data = file.read()
+
+    return render_template('index.html', csv_data=csv_data)
 
 
 if __name__ == '__main__':
