7ad003d5e336504ecb9db212cd153437239e8d02
njindal <jindal.nipun@gmail.com>
2023-06-18 15:36:19 +0530

    Fix create issue

--- app.py
+++ app.py
@@ -1,31 +1,38 @@
-# app.py
+from flask import Flask, render_template, request, jsonify, session
 import os
-from flask import Flask, render_template, request, jsonify
 import datetime
 import pytz
 import pandas as pd
+import json
 
 app = Flask(__name__)
-
-# Global variable for storing the CSV data
-csv_data = None
+app.secret_key = 'your_secret_key'  # Set a secret key for session management
 
 
 def read_csv_data(csv_path='data/data.csv'):
-    global csv_data
-
-    # Read CSV data from memory if it is already loaded
-    if csv_data is not None:
+    # Read CSV data from session if it exists
+    if 'csv_data' in session:
+        csv_data_json = session['csv_data']
+        csv_data = pd.read_json(csv_data_json)
         return csv_data
 
     # Read CSV data from disk
     csv_data = pd.read_csv(csv_path, keep_default_na=False)
+
+    # Store CSV data in session
+    csv_data_json = csv_data.to_json(orient='records')
+    session['csv_data'] = csv_data_json
+
     return csv_data
 
 
 def save_csv_data(csv_data, csv_path='data/data.csv'):
     # Save CSV data using Pandas
     csv_data.to_csv(csv_path, index=False)
+
+    # Update the CSV data in session
+    csv_data_json = csv_data.to_json(orient='records')
+    session['csv_data'] = csv_data_json
 
 
 def get_modified_date(csv_path='data/data.csv'):
@@ -50,8 +57,6 @@ def index():
 
 @app.route('/edit', methods=['POST'])
 def edit():
-    global csv_data
-
     # Retrieve the edited data from the request form
     row_index = int(request.form['editRowIndex'])
     osn_no = request.form['editOSNNo']
@@ -63,10 +68,13 @@ def edit():
     datetime_obj = datetime.datetime.strptime(next_date, "%Y-%m-%d")
     formatted_next_date = datetime_obj.strftime("%m/%d/%y 00:00:00")
 
+    # Read data from session
+    csv_data = read_csv_data()
+
     if row_index == -1:
         # Create a new row in the CSV data
         new_row = pd.DataFrame({
-            'index': csv_data.index.max() + 1,
+            'index': [csv_data.index.max() + 1],
             'OSN_NO': [osn_no],
             'Party_Name': [party_name],
             'Next_Date': [formatted_next_date],
@@ -80,13 +88,16 @@ def edit():
         csv_data.loc[row_index, 'Next_Date'] = formatted_next_date
         csv_data.loc[row_index, 'Work_Done'] = work_done
 
-    # Save the CSV data back to the file
+    # Save the CSV data back to the file and update session
     save_csv_data(csv_data)
+
+    # Convert DataFrame to list of dictionaries
+    csv_data_dict = csv_data.to_dict(orient='records')
 
     # Return the updated CSV data and modified date as JSON response
     response_data = {
         'status': 'success',
-        'csv_data': csv_data.to_dict(orient='records'),
+        'csv_data': csv_data_dict,
         'modified_date': get_modified_date()
     }
 
